f52e6472c46c5c38f2ca362e1574eceeeb4f6595
Voteraction Admin <arvind@voteraction.com>
2026-02-02 07:55:22 +0530

    Fix: Revert CSS & exclude binary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './main.css';
+import './globals.css';
 import { Inter, Noto_Sans_Devanagari } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
